b7a9ea63c27a8cfdbba7d55f8d24e03757b6c042
Julian Betancourt <julian@quiqup.com>
2018-05-21 21:09:40 +0200

    added billing and client side app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
+const bodyParser = require('body-parser')
 const keys = require('./config/keys')
 require('./models/User')
 require('./services/passport')
@@ -11,16 +12,37 @@ mongoose.connect(keys.mongoURI)
 // App
 const app = express()
 
-app.use(cookieSession({
-  maxAge: 30 * 24 * 60 * 1000, // 30 days in ms
-  keys: [keys.cookieKey]
-}))
+// Body Parser
+app.use(bodyParser.json())
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000, // 30 days in ms
+    keys: [keys.cookieKey],
+  })
+)
 
 app.use(passport.initialize())
 app.use(passport.session())
 
 // Auth Routes
 require('./routes/authRoutes')(app)
+
+// Billing Routes
+require('./routes/billingRoutes')(app)
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'))
+
+  // Express will serve up the index.html file
+  // if it doesnt recognize the route
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // Port
 const PORT = process.env.PORT || 5000
